2f3e78f55ea7576332e4b7c22291fb7a594f9d75
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-11 17:13:12 +0300

    Added function to check distance from hq

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(v1) {
+    let normLocation = 50;
+    let equal = 8;
+    if(v1 < normLocation) return 1;
+    if(v1 == normLocation) return equal;
+  }
